fcf519e73a757403229eadd9b4501e171da5ddfb
Yigit YILDIRIM <yigit.yildirim@enkoelektronik.com>
2018-01-24 13:01:09 +0300

    Server core tamamlandı
    incoming connection için handle işlemine başlanıcak.

--- gameserver/server-core.go
+++ gameserver/server-core.go
@@ -1,24 +1,59 @@
 package gameserver
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
-// Server yığını
-// Server bilgilerini tutar
+//ServerCore : Server yığını
 type ServerCore struct {
+	HostAddr       string       // Host soket adresi
+	ListenerSocket net.Listener // Dinlenen soket
 }
 
-// Bir server instance olusturup geri doner.
+//NewInstance : bir server nesnesi oluşturup geri döner.
 func NewInstance(address string) *ServerCore {
-	server := &ServerCore{}
+	server := &ServerCore{
+		HostAddr: address,
+	}
 
 	return server
 }
 
-// Serveri baslatir
+//Start : metodu serverı başlatır
 func (server *ServerCore) Start() {
 	fmt.Println("Server Starting...")
+	server.listen()
 }
 
+// Shutdown : metodu serverı kapatır
 func (server *ServerCore) Shutdown() {
 	fmt.Println("Server Shutdown!")
+
+}
+
+func (server *ServerCore) listen() {
+	l, err := net.Listen("tcp", server.HostAddr)
+	if err != nil {
+		fmt.Println("Error: ")
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
+	server.ListenerSocket = l
+
+	fmt.Printf("Socket is listening: %s \r\n", server.ListenerSocket.Addr())
+
+	for {
+		conn, err := server.ListenerSocket.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Incoming connection request <--")
+		// conn handla gönderilmeli
+		conn.RemoteAddr()
+	}
+
 }
